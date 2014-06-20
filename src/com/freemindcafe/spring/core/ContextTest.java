cd59a6bf6ddc0ef31a0076963f9795f51fa643a4
unknown <ic033920@AAEINGN536093L.ww888.siemens.net>
2014-06-20 19:51:19 +0530

    first commit

---
+++ src/com/freemindcafe/spring/core/ContextTest.java
@@ -0,0 +1,23 @@
+package com.freemindcafe.spring.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/com/freemindcafe/spring/core/ContextTest-context.xml"})
+public class ContextTest {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+ 
+	@Test
+	public void testContext() {
+	Assert.assertNotNull(applicationContext.getBean("test"));
+	}
+
+}
