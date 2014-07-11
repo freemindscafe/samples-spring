130b64750470fd3e47c6fe48ebb06689a1922a03
unknown <ic033920@AAEINGN536093L.ww888.siemens.net>
2014-07-11 22:19:08 +0530

    Added comments in the file

--- src/com/freemindcafe/SingleJUnitTestRunner.java
+++ src/com/freemindcafe/SingleJUnitTestRunner.java
@@ -9,6 +9,7 @@ import org.junit.runner.notification.RunListener;
 public class SingleJUnitTestRunner {
     public static void main(String... args) throws ClassNotFoundException {
         String[] classAndMethod = args[0].split("#");
+        //if we want to run a single test case. We should provide the TestClass#testMethod
         if(classAndMethod.length == 2){
 	        Request request = Request.method(Class.forName(classAndMethod[0]),
 	                classAndMethod[1]);
@@ -18,6 +19,7 @@ public class SingleJUnitTestRunner {
 	        Result result = jUnitCore.run(request);
 	        System.exit(result.wasSuccessful() ? 0 : 1);
         }
+        //otherwise run the normal Junit core
         else{
         	JUnitCore.main(args);
         }
